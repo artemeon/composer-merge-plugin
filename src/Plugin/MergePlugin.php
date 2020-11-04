2b6dddb30ae98be71a86ea7708b17b49ffd8ec70
Mike Marschall <mike.marschall@artemeon.de>
2020-11-04 14:37:38 +0100

    initialize module package loader in plugin entrypoint

--- src/Plugin/MergePlugin.php
+++ src/Plugin/MergePlugin.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Artemeon\Composer\Plugin;
 
+use Artemeon\Composer\Module\ModulePackageLoader;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
@@ -12,11 +13,13 @@ final class MergePlugin implements PluginInterface
 {
     private Composer $composer;
     private IOInterface $io;
+    private ModulePackageLoader $modulePackageLoader;
 
     public function activate(Composer $composer, IOInterface $io): void
     {
         $this->composer = $composer;
         $this->io = $io;
+        $this->modulePackageLoader = new ModulePackageLoader($io);
     }
 
     public function deactivate(Composer $composer, IOInterface $io): void
